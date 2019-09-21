ebc908939eebd0c55fb75b4a972c1b72774ddb38
Abby Bashore <abigail.bashore@slalom.com>
2019-09-21 10:31:10 -0600

    remove dev and prod stores (for now)

--- src/configureStore.js
+++ src/configureStore.js
@@ -1,6 +1,10 @@
-// Use CommonJS require below so we can dynamically import during build-time.
-if (process.env.NODE_ENV === "production") {
-  module.exports = require("./configureStore.prod");
-} else {
-  module.exports = require("./configureStore.dev");
+import { createStore } from "redux";
+import rootReducer from "./reducers/index";
+
+export default function configureStore(initialState) {
+
+  return createStore(
+    rootReducer,
+    initialState
+  );
 }
