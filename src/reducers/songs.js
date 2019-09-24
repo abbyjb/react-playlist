671d942f47bd93bd7cbce4bd0177b92485858f5c
abashore97 <bythebashore@gmail.com>
2019-09-24 08:07:23 -0600

    integrate local API into the application

--- src/reducers/songs.js
+++ src/reducers/songs.js
@@ -1,28 +1,24 @@
-const MOCK_STATE = [
-  {
-    id: 0, 
-    title: "Test Song",
-    artist: "Test Artist",
-    album: "Test Album"
-  }
-]
 
-
-export default function songs(state = MOCK_STATE, action) {
+export default function songs(state = [], action) {
   switch (action.type) {
-    case "ADD_SONG":
+    case "LOAD_SONGS_SUCCESS":
+      return action.songs;
+    case "ADD_SONG_SUCCESS":
       return [
         ...state,
         {
           id: state.reduce((maxId, todo) => Math.max(todo.id, maxId), -1) + 1,
-          title: action.title,
-          artist: action.artist,
-          album: action.album
+          title: action.song.title,
+          artist: action.song.artist,
+          album: action.song.album
         }
       ];
-    case "DELETE_SONG":
+    case "UPDATE_SONG_SUCCESS":
+      return state.map((song) => action.id === song.id ? song = action.song : song);
+    case "DELETE_SONG_SUCCESS":
       return state.filter(song => song.id !== action.id);
     default:
       return state;
   }
 }
+
