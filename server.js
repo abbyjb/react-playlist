b5e857451e1207a9db9298b8b07a3265023659e1
abbyjb <22553870+abbyjb@users.noreply.github.com>
2025-11-17 19:43:11 -0800

    replace dotenv with dotenv-flow

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
-require('dotenv').config({path: `.env.${process.env.NODE_ENV}`})
+
 const jsonServer = require('json-server')
 const path = require('path')
 const router = jsonServer.router(path.join(__dirname, 'db.json'))
 const server = jsonServer.create()
 const middlewares = jsonServer.defaults()
+require('dotenv-flow').config();
 
 server.use(middlewares)
 server.use(router)
 server.listen(process.env.SERVER_PORT, () => {
-  console.log('JSON Server is running')
+  console.log(`JSON Server is running at ${process.env.SERVER_PORT}`)
 
 })
